2b29b2c198d96ae1ce701b46fecd5e7e4e58887e
NEK1Tocik <gold81963@gmail.com>
2026-07-14 20:26:42 +0300

    fix serializers for accounts and add admin.py/requests.md

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_active', 'created_at')
+    list_filter = ('is_active', 'is_staff', 'is_superuser', 'created_at')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    ordering = ('-created_at',)     # ordering значит что при заходе на страницу аккаунты будут отсортированны в порядке создания "сначала новые", "потом самые старые"
+    
+    # группируем различные поля в админке для удобства
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'avatar', 'bio')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined', 'created_at', 'updated_at')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2'),
+        }),
+    )
+    
+    readonly_fields = ('created_at', 'updated_at')
